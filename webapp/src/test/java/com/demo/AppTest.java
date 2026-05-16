6f4df7e8772ce82fa9288addebf9befcb31132cf
Danish Ahmed <danishahmedfaraz@gmail.com>
2026-05-16 11:11:41 +0000

    Added Automated Tests

---
+++ webapp/src/test/java/com/demo/AppTest.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        assertEquals("CI/CD", "CI/CD");
+    }
+}
